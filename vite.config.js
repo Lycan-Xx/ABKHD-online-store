faa6a1fb15267cb1188d92c1c8dffb285e6d1366
Lycan-Xx <msbello514@gmail.com>
2025-01-11 15:09:19 +0100

    clearing the old and getting ready for the new

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-	plugins: [react()],
-	base: '/ABKHD-online-store/', // Add your repository name here
-});
