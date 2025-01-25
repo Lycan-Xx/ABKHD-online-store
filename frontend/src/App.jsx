3d2413bbfafd781df2ae6692654a303136e04d8f
Lycan-Xx <msbello514@gmail.com>
2025-01-25 20:55:07 +0100

    trying to fix routing failure in gihub pages 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Products from './components/Products';
@@ -9,8 +9,10 @@ import Contact from './components/Contact';
 import Admin from './pages/Admin';
 
 function App() {
+  const Router = import.meta.env.VITE_DEPLOY_TARGET === 'github' ? HashRouter : BrowserRouter;
+
   return (
-    <HashRouter>
+    <Router basename="/ABKHD-online-store">
       <Routes>
         <Route path="/admin" element={<Admin />} />
         <Route path="/" element={
@@ -24,7 +26,7 @@ function App() {
           </div>
         } />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
