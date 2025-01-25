fd84dd8955ef46256c91f1fbddeb0891fead7ad2
Lycan-Xx <msbello514@gmail.com>
2025-01-25 13:11:16 +0100

    Fixing port deployment issue 6

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ABKHD-online-store/',
+  server: {
+    port: process.env.PORT || 10000, // Use PORT from environment or default to 10000
+    host: '0.0.0.0', // Bind to all network interfaces
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})
+});
